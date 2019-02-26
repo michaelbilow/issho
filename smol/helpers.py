3cc26634252dc69e4efcfc7e21045ee6da21c946
Michael Bilow <michael.k.bilow@gmail.com>
2019-02-26 11:35:08 -0800

    update travis, clean up helpers

--- smol/helpers.py
+++ smol/helpers.py
@@ -9,7 +9,7 @@ def absolute_path(raw_path):
 
 
 def default_sftp_path(this_path, that_path):
-    return Path(this_path) if this_path else Path(that_path).name
+    return Path(this_path) if this_path else Path(Path(that_path).name)
 
 
 def able_to_connect(host, port, timeout=1.5):
