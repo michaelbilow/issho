456629a8f602e9d34755be2049092bd7227d6007
Michael Bilow <michael.k.bilow@gmail.com>
2019-03-25 16:47:14 -0700

    Bump version: 0.2.4 → 0.2.5

--- issho/__init__.py
+++ issho/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Michael Bilow"""
 __email__ = 'michael.k.bilow@gmail.com'
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 __docformat__ = 'restructuredtext'
 
 
--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/michaelbilow/issho',
-    version='0.2.4',
+    version='0.2.5',
     zip_safe=False,
 )
