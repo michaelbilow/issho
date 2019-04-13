42260896085d716fb59d4ead10540ae1f91b866f
Michael Bilow <michael.k.bilow@gmail.com>
2019-04-12 17:25:01 -0700

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/michaelbilow/issho',
-    version='0.3.0',
+    version='0.3.1',
     zip_safe=False,
 )
