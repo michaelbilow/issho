f924be4b4f6e93d5a9be949062d106495fd0e2fb
Michael Bilow <michael.k.bilow@gmail.com>
2019-05-23 10:49:42 -0700

    new docs

--- issho/config.py
+++ issho/config.py
@@ -19,6 +19,13 @@ def _make_issho_conf_dir():
 
 
 def read_issho_conf(profile, filename=ISSHO_CONF_FILE):
+    """
+    Writes issho variables out to a ``.toml`` file.
+
+    :param profile: The name of the profile to read
+    :param filename: The output filename
+    :return: a dict of data stored with that profile in the configuration file
+    """
     _make_issho_conf_dir()
     conf = toml.load(filename)
     if profile not in conf:
@@ -27,12 +34,19 @@ def read_issho_conf(profile, filename=ISSHO_CONF_FILE):
 
 
 def read_issho_env(profile):
+    """
+    Reads issho environment variables to a dict
+    :param profile: the name of the issho environment to draw from
+    :return: a dict of data with that profile stored in the environment file
+    """
     return read_issho_conf(profile, filename=ISSHO_ENV_FILE)
 
 
 def write_issho_conf(new_conf_dict, filename=ISSHO_CONF_FILE):
     """
     Updates the issho config file
+    :param new_conf_dict: the new configuration to add
+    :param filename: the location of the old configuration file
     """
     _make_issho_conf_dir()
     old_issho_conf = toml.load(filename)
@@ -42,6 +56,10 @@ def write_issho_conf(new_conf_dict, filename=ISSHO_CONF_FILE):
 
 
 def write_issho_env(new_env_dict):
+    """
+    Save a new issho environment
+    :param new_env_dict: the new set of environment paramters to add
+    """
     write_issho_conf(new_env_dict, filename=ISSHO_ENV_FILE)
 
 
